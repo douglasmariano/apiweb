7d63bddcfccf0dc4c81ff1ddfbc4b3950118a763
informatica <informatica@SUPORTE01.ajelmat.local>
2018-02-16 18:24:33 -0200

    Aula 4 part 1

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,9 @@
+//criação do modulo principal da aplicaçã
+var appCliente = angular.module("appCLiente",[]);
+
+//criação e controller
+appCliente.controller("contatoController", function(){ });
+
+appCliente.controller("cadastroController", function(){ });
+
+appCliente.controller("bemvindoController", function(){ });
