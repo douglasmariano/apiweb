0e29b150b3aa6b1e7e7ea899bdc9835f1552cc44
informatica <informatica@SUPORTE01.ajelmat.local>
2018-02-22 16:44:30 -0300

    Começa da aula 5

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,9 +1,56 @@
 //criação do modulo principal da aplicaçã
-var appCliente = angular.module("appCLiente",[]);
+var appCliente = angular.module("appCliente", []);
 
-//criação e controller
-appCliente.controller("contatoController", function(){ });
+// criação e controller
+appCliente.controller("indexController", function($scope, $http) {
 
-appCliente.controller("cadastroController", function(){ });
+	$scope.clientes = [];
+	$scope.cliente = {};
 
-appCliente.controller("bemvindoController", function(){ });
+	$scope.carregarClientes = function() {
+		$http({
+			method : 'GET',
+			url : 'http://localhost:3000/clientes'
+		}).then(function(response) {
+			$scope.clientes = response.data;
+			console.log(response.data);
+			console.log(response.status);
+
+		}, function(response) {
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+
+	$scope.salvarCliente = function() {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:3000/clientes',
+			data : $scope.cliente
+		}).then(function(response) {
+			$scope.clientes.push(response.data);
+
+		}, function(response) {
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+	
+	$scope.excluirCliente = function(cliente) {
+		$http({
+			method : 'DELETE',
+			url : 'http://localhost:3000/clientes/'+ cliente.id
+		}).then(function(response) {
+			
+			pos = $scope.clientes.indexOf(cliente);
+			$scope.clientes.splice(pos, 1);
+			
+
+		}, function(response) {
+			console.log(response.data);
+			console.log(response.status);
+		});
+	};
+	$scope.carregarClientes();
+
+});
